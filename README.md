165317d5c57d105cde8f21c4660b37450e9717de
BrandonFL <github@brandonfl.dev>
2025-09-18 17:13:14 +0200

    clean code

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Spring lombok issue
 
+## Used versions
+
+- Spring Boot 3.1.4
+- Lombok 1.18.30
+- Java 17
+
+## Run
+
+```bash
+docker compose up --build --force-recreate
+```
+
 ## Expected Behavior
 
 On http://localhost:8080/
--- src/main/java/dev/brandonfl/bugtest/TestDTO.java
+++ src/main/java/dev/brandonfl/bugtest/TestDTO.java
@@ -3,16 +3,12 @@ package dev.brandonfl.bugtest;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 import java.io.Serializable;
 
 @Getter
 @Setter
-@ToString
-@NoArgsConstructor
 @AllArgsConstructor
 public class TestDTO implements Serializable {
 
